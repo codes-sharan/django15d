5ce508d5dba160757aae4850eccc0dd3e430207b
Sharan Panthi <shpanthi09@gmail.com>
2024-09-28 00:02:11 +0545

    login/reg/protected content

--- blog/views/main_view.py
+++ blog/views/main_view.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from ..models import Blogs
+from django.contrib.auth.decorators import login_required
+
 
 def home(request):
     return render(request,'main/home.html')
@@ -9,5 +12,17 @@ def single_blog(request):
 def edit_blog(request):
     return render(request,"main/edit_blog.html")
 
+@login_required
 def create_blog(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        subtitle =  request.POST.get("subtitle")
+        description = request.POST.get("description")
+        image = request.FILES.get("image")
+        blog = Blogs(title=title,subtitle=subtitle,description=description,image=image)
+        blog.save()
+        return redirect("home")
+  
     return render(request,"main/create_blog.html")
+
+        
